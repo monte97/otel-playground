333b4273f17271e69ef83675c0993d8afe21d3ff
monte97 <francesco@montelli.dev>
2025-02-19 22:19:48 +0100

    chore(env): clenaup

--- configs/tempo/setup.sh
+++ configs/tempo/setup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-
-echo ${OTEL_COLLECTOR_HOST}
-
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
 # Define the path to the template and the output file
 TEMPLATE_FILE="./configs/tempo/tempo-config.yaml.template"
 OUTPUT_FILE="./configs/tempo/tempo-config.yaml"
 
 # Replace placeholders with environment variable values and write to the output file
-sed "s|\${OTEL_COLLECTOR_HOST}|${OTEL_COLLECTOR_HOST}|g" $TEMPLATE_FILE | \
-sed "s|\${OTEL_COLLECTOR_PORT_GRPC}|${OTEL_COLLECTOR_PORT_GRPC}|g" | \
-sed "s|\${OTEL_COLLECTOR_PORT_HTTP}|${OTEL_COLLECTOR_PORT_HTTP}|g" | \
+sed "s|\${TEMPO_PORT_HTTP}|${TEMPO_PORT_HTTP}|g" $TEMPLATE_FILE | \
+sed "s|\${TEMPO_PORT_OLTP}|${TEMPO_PORT_OLTP}|g" | \
 sed "s|\${PROMETHEUS_HOST}|${PROMETHEUS_HOST}|g" | \
 sed "s|\${PROMETHEUS_PORT}|${PROMETHEUS_PORT}|g" > $OUTPUT_FILE
