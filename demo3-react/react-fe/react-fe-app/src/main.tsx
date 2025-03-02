83cafcebf4c0d2fea563f2038f17abdf7f1d5e8a
monte97 <francesco@montelli.dev>
2025-03-02 19:50:08 +0100

    save

--- demo3-react/react-fe/react-fe-app/src/main.tsx
+++ demo3-react/react-fe/react-fe-app/src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Faro SDK imports
+import { initializeFaro } from '@grafana/faro-web-sdk';
+import { getWebInstrumentations } from '@grafana/faro-web-sdk';
+
+initializeFaro({
+  url: 'http://192.168.0.138:4318/v1/traces', // Replace with your OTel Collector endpoint
+  app: {
+    name: 'my-react-app',
+    version: '1.0.0',
+    environment: 'development',
+  },
+  instrumentations: [
+    ...getWebInstrumentations(),
+  ],
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
